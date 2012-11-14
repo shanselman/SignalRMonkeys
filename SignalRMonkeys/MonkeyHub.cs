3d33aec07c90547c1098a3c80e134530d2e56f7f
Scott Hanselman <scott@hanselman.com>
2012-11-14 13:49:20 -0800

    initial

---
+++ SignalRMonkeys/MonkeyHub.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SignalRMonkeys
+{
+    [HubName("monkeys")]
+    public class MonkeyHub : Hub
+    {
+        private static Task currentTask = Task.FromResult(0);
+        private static CancellationTokenSource cancellation = new CancellationTokenSource();
+
+        public void StartTyping(string targetText, bool parallel)
+        {
+            var settings = new GeneticAlgorithmSettings { PopulationSize = 200 };
+            var token = cancellation.Token;
+
+            
+            currentTask = currentTask.ContinueWith((previous) =>
+            {
+                // Create the new genetic algorithm
+                var ga = new TextMatchGeneticAlgorithm(parallel, targetText, settings);
+                TextMatchGenome? bestGenome = null;
+                DateTime startedAt = DateTime.Now;
+
+                Hub.GetClients<MonkeyHub>().started(targetText);
+
+                // Iterate until a solution is found or until cancellation is requested
+                for (int generation = 1; ; generation++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Hub.GetClients<MonkeyHub>().cancelled();
+                        break;
+                    }
+
+                    // Move to the next generation
+                    ga.MoveNext();
+
+                    // If we've found the best solution thus far, update the UI
+                    if (bestGenome == null ||
+                        ga.CurrentBest.Fitness < bestGenome.Value.Fitness)
+                    {
+                        bestGenome = ga.CurrentBest;
+                        
+                        int generationsPerSecond = generation / Math.Max(1, (int)((DateTime.Now - startedAt).TotalSeconds));
+                        Hub.GetClients<MonkeyHub>().updateProgress(bestGenome.Value.Text, generation, generationsPerSecond);
+
+                        if (bestGenome.Value.Fitness == 0)
+                        {
+                            Hub.GetClients<MonkeyHub>().complete();
+                            break;
+                        }
+                    }
+                }                
+            }, TaskContinuationOptions.OnlyOnRanToCompletion);
+        }
+
+        public void StopTyping()
+        {
+            cancellation.Cancel();
+            cancellation = new CancellationTokenSource();
+            currentTask = Task.FromResult(0);
+        }
+        
+    }
+}
---
+++ SignalRMonkeys/ThreadSafeRandom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SignalRMonkeys
+{
+    public sealed class ThreadSafeRandom : Random
+    {
+        private object _lock = new object();
+
+        public override int Next()
+        {
+            lock (_lock) return base.Next();
+        }
+
+        public override int Next(int maxValue)
+        {
+            lock (_lock) return base.Next(maxValue);
+        }
+
+        public override int Next(int minValue, int maxValue)
+        {
+            lock (_lock) return base.Next(minValue, maxValue);
+        }
+
+        public override void NextBytes(byte[] buffer)
+        {
+            lock (_lock) base.NextBytes(buffer);
+        }
+
+        public override double NextDouble()
+        {
+            lock (_lock) return base.NextDouble();
+        }
+    }
+}
